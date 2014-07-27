f9973392b5011b3a31960d952e81425844c1e59d
Michele <micxgx@gmail.com>
2014-07-27 23:18:01 +0100

    TMP - Initial draft.

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/* Custom scripts and plugins initialization
+
+*/
+
+// Initialize smooth scrolling
+// Automatically add data-scroll attribute as required by the plugin to all internall links
+
+$(function() {
+    $('a[href^=#]').attr('data-scroll', '');
+
+    smoothScroll.init({
+    });
+
+});
+
+/* hide navbar collapsed (mobile view) when clicking a link
+ * http://stackoverflow.com/questions/16680543/hide-twitter-bootstrap-nav-collapse-on-click
+ */
+$(function() {
+    $('.nav a, #home').on('click', function(){
+        if($('.navbar-toggle').css('display') !='none' &&  $('#bs-example-navbar-collapse-1').hasClass('in') ) {
+            setTimeout(function () { $(".navbar-toggle").trigger( "click" ) },
+                250);
+        }
+    });
+});
+
+
